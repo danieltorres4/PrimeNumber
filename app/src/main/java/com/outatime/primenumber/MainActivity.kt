229548aebb05fba0cc5eac7798f329326abda7a7
danieltorres4 <dst990502@outlook.com>
2022-11-01 18:49:28 -0600

    Adding new font families and the algorithm to verify if a number is prime or not

--- app/src/main/java/com/outatime/primenumber/MainActivity.kt
+++ app/src/main/java/com/outatime/primenumber/MainActivity.kt
@@ -2,6 +2,7 @@ package com.outatime.primenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.outatime.primenumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
 
+    fun click(view: View) {
+        val number = binding.etNumber.text.toString().toInt()
+
+        if(isItPrime(number)){
+            binding.tvResult.text = "Number $number is prime!"
+        } else {
+            binding.tvResult.text = "Number $number is not prime!"
+        }
+    }
+
+    //Algorithm to verify if a number is prime or not
+    fun isItPrime(number: Int): Boolean {
+        if(number <= 1) return false
+
+        for(i in 2 until number){
+            if(number % i == 0) return false
+        }
+
+        return true
     }
 }
