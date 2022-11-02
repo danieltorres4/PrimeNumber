c35b6e07f91ca89c0dad3f36686cc3be6df6c11b
danieltorres4 <dst990502@outlook.com>
2022-11-01 18:30:05 -0600

    First commit

---
+++ app/src/main/java/com/outatime/primenumber/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.outatime.primenumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.outatime.primenumber.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
